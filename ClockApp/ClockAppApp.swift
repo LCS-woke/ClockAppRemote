63a0157d9f62dafb3b09a15b10d1eb7c3c4028ae
Will Oke <Woke@lcs.on.ca>
2023-11-03 10:57:17 -0400

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by William Oke on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
